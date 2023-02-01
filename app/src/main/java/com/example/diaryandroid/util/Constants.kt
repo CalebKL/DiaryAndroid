b06e151db232e687ff28eb391028dfd5feb767f6
Caleb Langat <95022986+Mzazi25@users.noreply.github.com>
2023-02-01 20:57:03 +0300

    Updated constants with client and app ID

---
+++ app/src/main/java/com/example/diaryandroid/util/Constants.kt
@@ -0,0 +1,7 @@
+package com.example.diaryandroid.util
+
+object Constants {
+
+    const val APP_ID = "mydiary-usilf"
+    const val CLIENT_ID = "417379619294-bfve7ovnj2vmm36e1ho4mtvbet1sec2i.apps.googleusercontent.com"
+}
