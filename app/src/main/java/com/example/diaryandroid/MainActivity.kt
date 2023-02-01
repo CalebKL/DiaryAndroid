c1d1ce6c57b038a890f387400d1ea8eb703554d7
Caleb Langat <95022986+Mzazi25@users.noreply.github.com>
2023-02-01 13:54:42 +0300

    Added project dependencies

--- app/src/main/java/com/example/diaryandroid/MainActivity.kt
+++ app/src/main/java/com/example/diaryandroid/MainActivity.kt
@@ -17,27 +17,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             DiaryAndroidTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    DiaryAndroidTheme {
-        Greeting("Android")
     }
 }
