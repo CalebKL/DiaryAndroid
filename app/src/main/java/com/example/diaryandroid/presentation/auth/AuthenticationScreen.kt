5f60fa9bb86b50406c5e06c81a53c6591f9182e9
Caleb Langat <95022986+Mzazi25@users.noreply.github.com>
2023-02-02 20:59:30 +0300

    Added authentication view model and screen update

--- app/src/main/java/com/example/diaryandroid/presentation/auth/AuthenticationScreen.kt
+++ app/src/main/java/com/example/diaryandroid/presentation/auth/AuthenticationScreen.kt
@@ -1,28 +1,68 @@
 package com.example.diaryandroid.presentation.auth
 
+import android.util.Log
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.diaryandroid.presentation.auth.components.AuthenticationContent
+import com.example.diaryandroid.presentation.auth.components.AuthenticationViewModel
+import com.example.diaryandroid.util.Constants.CLIENT_ID
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import com.stevdzasan.messagebar.ContentWithMessageBar
+import com.stevdzasan.messagebar.MessageBarState
+import com.stevdzasan.messagebar.rememberMessageBarState
+import com.stevdzasan.onetap.OneTapSignInState
+import com.stevdzasan.onetap.OneTapSignInWithGoogle
+import com.stevdzasan.onetap.rememberOneTapSignInState
+import timber.log.Timber
+import java.lang.Exception
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 @Destination
 fun AuthenticationScreen(
-    navigator: DestinationsNavigator?
+    navigator: DestinationsNavigator?,
+    oneTapSignInState: OneTapSignInState = rememberOneTapSignInState(),
+    messageBarState: MessageBarState = rememberMessageBarState(),
+    viewModel: AuthenticationViewModel = hiltViewModel(),
 ) {
+    val loadingState by viewModel.loadingState
     Scaffold(
         content = {
             val unUsedPadding = it
-            AuthenticationContent(
-                onButtonClicked = {
-
+            ContentWithMessageBar(messageBarState = messageBarState) {
+                AuthenticationContent(
+                    onButtonClicked = {
+                        oneTapSignInState.open()
+                        viewModel.setLoading(true)
+                    },
+                    loadingState = loadingState
+                )
+            }
+        }
+    )
+    OneTapSignInWithGoogle(
+        state = oneTapSignInState,
+        clientId =CLIENT_ID ,
+        onTokenIdReceived = { tokenId ->
+            viewModel.signInWithMongoAtlas(
+                tokenId= tokenId,
+                onSuccess = {
+                    if (it){
+                        messageBarState.addSuccess("Successfully, Authenticated!")
+                    }
                 },
-                loadingState = false
+                onError = {
+                    messageBarState.addError(it)
+                }
             )
+        },
+        onDialogDismissed = {message->
+            messageBarState.addError(Exception(message))
         }
     )
 }
---
+++ app/src/main/java/com/example/diaryandroid/presentation/auth/components/AuthenticationViewModel.kt
@@ -0,0 +1,45 @@
+package com.example.diaryandroid.presentation.auth.components
+
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.diaryandroid.util.Constants.APP_ID
+import io.realm.kotlin.mongodb.App
+import io.realm.kotlin.mongodb.Credentials
+import io.realm.kotlin.mongodb.GoogleAuthType
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class AuthenticationViewModel: ViewModel() {
+    var loadingState = mutableStateOf(false)
+        private set
+
+    fun setLoading(loading:Boolean){
+        loadingState.value = loading
+    }
+
+    fun signInWithMongoAtlas(
+        tokenId:String,
+        onSuccess:(Boolean)->Unit,
+        onError:(Exception)->Unit
+    ){
+        viewModelScope.launch {
+            try {
+                val result = withContext(Dispatchers.IO){
+                    App.create(APP_ID).login(
+                        credentials = Credentials.google(tokenId, GoogleAuthType.ID_TOKEN)
+                    ).loggedIn
+                }
+                withContext(Dispatchers.Main){
+                    onSuccess(result)
+                }
+
+            }catch (e:Exception){
+                withContext(Dispatchers.Main){
+                    onError(e)
+                }
+            }
+        }
+    }
+}
