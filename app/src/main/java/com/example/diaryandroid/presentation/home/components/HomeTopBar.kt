cf3313b8698d9c7a459265b3493de247a868fbce
Caleb Langat <95022986+Mzazi25@users.noreply.github.com>
2023-02-03 15:37:32 +0300

    Added home top bar

---
+++ app/src/main/java/com/example/diaryandroid/presentation/home/components/HomeTopBar.kt
@@ -0,0 +1,37 @@
+package com.example.diaryandroid.presentation.home.components
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import com.example.diaryandroid.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeTopBar(
+    onMenuClicked:()->Unit
+) {
+    TopAppBar(
+        navigationIcon = {
+            IconButton(onClick = { onMenuClicked() }) {
+                Icon(
+                    imageVector = Icons.Default.Menu,
+                    contentDescription = stringResource(R.string.hamburger_menu_icon)
+                )
+            }
+        },
+        title = {
+            Text(text = stringResource(R.string.diary))
+        },
+        actions = {
+            IconButton(onClick = { onMenuClicked() }) {
+                Icon(
+                    imageVector = Icons.Default.DateRange,
+                    contentDescription = stringResource(R.string.date_range)
+                )
+            }
+        }
+    )
+}
