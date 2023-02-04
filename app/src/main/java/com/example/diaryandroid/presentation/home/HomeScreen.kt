6310e4b1ed3622c0fe7b17686ce12c7775ebdcf5
Caleb Langat <95022986+Mzazi25@users.noreply.github.com>
2023-02-04 23:03:07 +0300

    updated home screen

--- app/src/main/java/com/example/diaryandroid/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/example/diaryandroid/presentation/home/HomeScreen.kt
@@ -2,10 +2,17 @@ package com.example.diaryandroid.presentation.home
 
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.example.diaryandroid.R
+import com.example.diaryandroid.presentation.home.components.HomeTopBar
 import com.ramcosta.composedestinations.annotation.Destination
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -16,6 +23,19 @@ fun HomeScreen() {
         modifier = Modifier
             .navigationBarsPadding()
             .statusBarsPadding(),
+        topBar = {
+            HomeTopBar(
+                onMenuClicked = {}
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(onClick = {}) {
+                Icon(
+                    imageVector = Icons.Default.Edit,
+                    contentDescription = stringResource(R.string.new_diary_icon)
+                )
+            }
+        },
         content = {
             val unUsedPadding = it
         }
