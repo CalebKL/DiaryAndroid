87d3dc58c043f077ecb6e0dba6abd2077ce2f68c
Caleb Langat <95022986+Mzazi25@users.noreply.github.com>
2023-02-06 16:21:09 +0300

    Added model classes and vector files

---
+++ app/src/main/java/com/example/diaryandroid/model/Diary.kt
@@ -0,0 +1,19 @@
+package com.example.diaryandroid.model
+
+import io.realm.kotlin.types.annotations.PrimaryKey
+import io.realm.kotlin.ext.realmListOf
+import io.realm.kotlin.types.ObjectId
+import io.realm.kotlin.types.RealmInstant
+import io.realm.kotlin.types.RealmList
+import io.realm.kotlin.types.RealmObject
+
+open class Diary : RealmObject {
+    @PrimaryKey
+    var _id: ObjectId = ObjectId.create()
+    var ownerId: String = ""
+    var title: String = ""
+    var description: String = ""
+    var images: RealmList<String> = realmListOf()
+    var date: RealmInstant = RealmInstant.from(System.currentTimeMillis(), 0)
+
+}
---
+++ app/src/main/java/com/example/diaryandroid/model/Mood.kt
@@ -0,0 +1,11 @@
+package com.example.diaryandroid.model
+
+import androidx.compose.ui.graphics.Color
+
+enum class Mood(
+    val icon: Int,
+    val contentColor: Color,
+    val containerColor: Color
+) {
+
+}
