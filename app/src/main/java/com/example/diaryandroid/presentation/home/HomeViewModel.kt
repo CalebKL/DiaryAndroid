59e714c9cca8a1f6fabf3a7402258ecbcf2089f2
Caleb Langat <95022986+Mzazi25@users.noreply.github.com>
2023-02-08 19:38:29 +0300

    Added home viewmodel

---
+++ app/src/main/java/com/example/diaryandroid/presentation/home/HomeViewModel.kt
@@ -0,0 +1,24 @@
+package com.example.diaryandroid.presentation.home
+
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.diaryandroid.data.Diaries
+import com.example.diaryandroid.data.MongoDB
+import com.example.diaryandroid.util.Resource
+import kotlinx.coroutines.launch
+
+class HomeViewModel:ViewModel() {
+    var diaries: MutableState<Diaries> = mutableStateOf(Resource.Loading())
+    init {
+        observeAllDiaries()
+    }
+    private fun observeAllDiaries(){
+       viewModelScope.launch {
+           MongoDB.getAllDiaries().collect{result->
+               diaries.value = result
+           }
+       }
+    }
+}
