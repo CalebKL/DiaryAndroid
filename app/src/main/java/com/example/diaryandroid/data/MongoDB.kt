93b5eda35aad5011544af95ddffc1ea221abf364
Caleb Langat <95022986+Mzazi25@users.noreply.github.com>
2023-02-08 19:27:31 +0300

    Updated MongoDB repo and implementation

--- app/src/main/java/com/example/diaryandroid/data/MongoDB.kt
+++ app/src/main/java/com/example/diaryandroid/data/MongoDB.kt
@@ -2,16 +2,27 @@ package com.example.diaryandroid.data
 
 import com.example.diaryandroid.model.Diary
 import com.example.diaryandroid.util.Constants.APP_ID
+import com.example.diaryandroid.util.Resource
+import com.example.diaryandroid.util.toInstant
 import io.realm.kotlin.Realm
 import io.realm.kotlin.ext.query
 import io.realm.kotlin.log.LogLevel
 import io.realm.kotlin.mongodb.App
 import io.realm.kotlin.mongodb.sync.SyncConfiguration
+import io.realm.kotlin.query.Sort
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
+import java.time.ZoneId
 
 object MongoDB: MongoRepository {
     private val app = App.create(APP_ID)
     private val user = app.currentUser
     private lateinit var realm: Realm
+
+    init {
+        configureTheRealm()
+    }
 
     override fun configureTheRealm() {
         if (user!= null){
@@ -25,6 +36,33 @@ object MongoDB: MongoRepository {
                 .log(LogLevel.ALL)
                 .build()
             realm = Realm.open(config)
+        }
+    }
+
+    override fun getAllDiaries(): Flow<Diaries> {
+       return if (user != null){
+            try {
+                realm.query<Diary>(query = "ownerId ==$0", user.identity)
+                    .sort(property = "date", sortOrder = Sort.DESCENDING)
+                    .asFlow().map {result->
+                        Resource.Success(
+                            data = result.list.groupBy {
+                                it.date.toInstant().atZone(
+                                    ZoneId.systemDefault()
+                                ).toLocalDate()
+                            }
+                        )
+                    }
+            }catch (e:Exception){
+                flow {
+                    emit(Resource.Error(message = e.localizedMessage?:"Exception thrown: Invalid"))
+
+                }
+            }
+        }else{
+            flow {
+                emit(Resource.Error(message = "User is not Logged in"))
+            }
         }
     }
 }
--- app/src/main/java/com/example/diaryandroid/data/MongoRepository.kt
+++ app/src/main/java/com/example/diaryandroid/data/MongoRepository.kt
@@ -1,5 +1,13 @@
 package com.example.diaryandroid.data
 
+import com.example.diaryandroid.model.Diary
+import com.example.diaryandroid.util.Resource
+import kotlinx.coroutines.flow.Flow
+import java.time.LocalDate
+
+typealias Diaries = Resource<Map<LocalDate, List<Diary>>>
+
 interface MongoRepository {
     fun configureTheRealm()
+    fun getAllDiaries():Flow<Diaries>
 }
