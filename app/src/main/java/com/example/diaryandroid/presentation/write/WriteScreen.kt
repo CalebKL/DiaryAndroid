a4c2ef98e04a6321e4d135d5297e4f4ed3b9717a
Caleb Langat <95022986+Mzazi25@users.noreply.github.com>
2023-02-09 10:17:52 +0300

    Added write content and updated write screen

--- app/src/main/java/com/example/diaryandroid/presentation/write/WriteScreen.kt
+++ app/src/main/java/com/example/diaryandroid/presentation/write/WriteScreen.kt
@@ -1,19 +1,25 @@
 package com.example.diaryandroid.presentation.write
 
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import com.example.diaryandroid.model.Diary
+import com.example.diaryandroid.presentation.write.components.WriteContent
 import com.example.diaryandroid.presentation.write.components.WriteTopBar
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.rememberPagerState
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalPagerApi::class)
 @Composable
 @Destination
 fun WriteScreen(
     navigator: DestinationsNavigator?
 ) {
+    val pagerState = rememberPagerState()
+    val scrollState = rememberScrollState()
     Scaffold(
         topBar = {
                  WriteTopBar(
@@ -25,7 +31,14 @@ fun WriteScreen(
                  )
         },
         content = {
-            val unUsedPadding = it
+            WriteContent(
+                paddingValues = it,
+                scrollState = scrollState,
+                pagerState = pagerState,
+                title = "",
+                onTitleChanged = {},
+                onDescriptionChanged = {}
+            )
         }
     )
 }
---
+++ app/src/main/java/com/example/diaryandroid/presentation/write/components/WriteContent.kt
@@ -0,0 +1,116 @@
+package com.example.diaryandroid.presentation.write.components
+
+import Mood
+import androidx.compose.foundation.ScrollState
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import com.example.diaryandroid.R
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.PagerState
+import com.google.accompanist.pager.rememberPagerState
+
+@OptIn(ExperimentalPagerApi::class, ExperimentalMaterial3Api::class)
+@Composable
+fun WriteContent(
+    paddingValues: PaddingValues,
+    scrollState: ScrollState,
+    pagerState: PagerState,
+    title:String,
+    onTitleChanged:(String)->Unit,
+    onDescriptionChanged:(String)->Unit
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = paddingValues.calculateTopPadding())
+            .padding(bottom = paddingValues.calculateBottomPadding())
+            .padding(bottom = 24.dp)
+            .padding(horizontal = 24.dp),
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .verticalScroll(state = scrollState))
+        {
+            Spacer(modifier = Modifier.height(30.dp))
+            HorizontalPager(
+                state = pagerState,
+                count = Mood.values().size)
+            {page->
+                AsyncImage(
+                    modifier= Modifier.size(120.dp),
+                    model = ImageRequest.Builder(LocalContext.current)
+                        .data(Mood.values()[page].icon)
+                        .crossfade(true)
+                        .build(),
+                    contentDescription = stringResource(R.string.mood_image)
+                )
+            }
+            Spacer(modifier = Modifier.height(30.dp))
+            TextField(
+                modifier = Modifier.fillMaxWidth(),
+                value = title,
+                onValueChange = onTitleChanged, 
+                placeholder = { Text(text = "Title") },
+                colors = TextFieldDefaults.textFieldColors(
+                    containerColor = Color.Transparent,
+                    focusedIndicatorColor = Color.Unspecified,
+                    disabledIndicatorColor = Color.Unspecified,
+                    unfocusedIndicatorColor = Color.Unspecified,
+                    placeholderColor = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.38f)
+                ),
+                keyboardOptions = KeyboardOptions(
+                    imeAction = ImeAction.Next
+                ),
+                keyboardActions = KeyboardActions(
+                    onNext = {}
+                ),
+                maxLines = 1,
+                singleLine = true
+            )
+            TextField(
+                modifier = Modifier.fillMaxWidth(),
+                value = title,
+                onValueChange = onDescriptionChanged,
+                placeholder = { Text(text = "Tell me about it") },
+                colors = TextFieldDefaults.textFieldColors(
+                    containerColor = Color.Transparent,
+                    focusedIndicatorColor = Color.Unspecified,
+                    disabledIndicatorColor = Color.Unspecified,
+                    unfocusedIndicatorColor = Color.Unspecified,
+                    placeholderColor = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.38f)
+                ),
+                keyboardOptions = KeyboardOptions(
+                    imeAction = ImeAction.Next
+                ),
+                keyboardActions = KeyboardActions(
+                    onNext = {}
+                )
+            )
+        }
+        Column(verticalArrangement = Arrangement.Bottom) {
+            Spacer(modifier = Modifier.height(12.dp))
+            Button(
+                modifier = Modifier.fillMaxWidth().height(54.dp),
+                shape = MaterialTheme.shapes.small,
+                onClick = { /*TODO*/ }) {
+                Text(text = "Save")
+            }
+        }
+    }
+}
