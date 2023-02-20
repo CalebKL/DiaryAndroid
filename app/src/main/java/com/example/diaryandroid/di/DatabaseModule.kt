81cbbb767a4bfd74da84aad46b68cc640137b23f
Caleb Langat <95022986+Mzazi25@users.noreply.github.com>
2023-02-20 21:45:36 +0300

    Added dependency Injection

---
+++ app/src/main/java/com/example/diaryandroid/di/DatabaseModule.kt
@@ -0,0 +1,33 @@
+package com.example.diaryandroid.di
+
+import android.content.Context
+import androidx.room.Room
+import com.example.diaryandroid.data.database.ImagesDatabase
+import com.example.diaryandroid.util.Constants.IMAGES_DATABASE
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+    @Provides
+    @Singleton
+    fun providesDatabase(
+        @ApplicationContext context: Context
+    ):ImagesDatabase{
+        return Room.databaseBuilder(
+            context = context,
+            klass = ImagesDatabase::class.java,
+            name = IMAGES_DATABASE
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun providesFirstDao(database: ImagesDatabase) = database.imagesToUploadDao()
+
+}
